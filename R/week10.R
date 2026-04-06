0d375f7c06f34446fb2dc350b4b5e260a414f5ea
Patrick Mcgonagle <mcgon046@umn.edu>
2026-04-05 23:32:17 -0500

    save

--- R/week10.R
+++ R/week10.R
@@ -9,8 +9,8 @@ gss_tbl <- read_sav("../data/GSS2016.sav") |> # read_sav turns dataset into tibb
   drop_na(mosthrs) |># Dropping missing values here as per assignment 
   mutate(mosthrs = as.integer(mosthrs)) |> # Set these to integer values instead of doubles 
   select(-hrs1, -hrs2) |> # Getting rid of these variables as per assignment 
-  select(where(~ mean(is.na(.)) < 0.75)) # This call takes the piped data set, calcs the proportion of missingness (mean) and makes sure it is less than 0.75
-
+  select(where(~ mean(is.na(.)) < 0.75)) |># This call takes the piped data set, calcs the proportion of missingness (mean) and makes sure it is less than 0.75
+  mutate(across(everything(), as.numeric)) # So everything in the data set is numeric for ML tasks 
 # Visualization 
 gss_tbl |> 
   ggplot(aes(x = mosthrs)) + 
@@ -24,3 +24,28 @@ gss_tbl |>
   theme_minimal()
 
 # Analysis 
+
+## Training and test sets 
+set.seed(42) # You know, the universal answer to the meaning of life (setting seed for reproducability)
+index <- createDataPartition(gss_tbl$mosthrs, p = 0.75, list = FALSE) # Created index to split up data 
+train_data <- gss_tbl[index, ] # Training data set
+test_data <- gss_tbl[-index, ] # Testing data set
+
+## 10 fold cross-validation 
+cv_ten <- trainControl(
+  method = "cv", # Set for cross validation 
+  number = 10 # sets number of folds 
+)
+
+## Different models to test 
+### Impute (saves time)
+medimp <- "medianImpute"
+
+ols_model <- train(
+  mosthrs ~ ., # Most hours predicted from everything else
+  data = train_data, # Only using training data as is convention
+  method = "lm", #"lm" for OLS model 
+  preProcess = medimp, #Median imputation 
+  trControl = cv_ten, # Used preset cross-validation from above
+  na.action = na.pass #allows the NA
+)
