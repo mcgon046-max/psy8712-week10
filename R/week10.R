f0dc2347d387c35727f796a8f3adafbc89bd7cad
Patrick Mcgonagle <mcgon046@umn.edu>
2026-04-05 22:51:59 -0500

    first

---
+++ R/week10.R
@@ -0,0 +1,8 @@
+# Script Settings and Resources 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(haven) # For SPSS read in 
+library(tidyverse) # For basic tidy packages
+library(caret) # For Machine learning tasks 
+
+
+
